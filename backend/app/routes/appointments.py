22621b1c1d2f8f471fb62ea4a510d5052a1e44aa
EC2 Default User <ec2-user@ip-172-31-29-36.ec2.internal>
2026-03-17 10:38:29 +0000

    fix: appointment API + docker compose config

--- backend/app/routes/appointments.py
+++ backend/app/routes/appointments.py
@@ -1,37 +1,36 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from pydantic import BaseModel
 from ..database.db import get_db
 from ..models.appointment import Appointment
 
 router = APIRouter()
 
+class AppointmentCreate(BaseModel):
+    patient_name: str
+    doctor_name: str
+    date: str
+
 
 @router.post("/appointment")
 def create_appointment(
-        patient_name: str,
-        doctor_name: str,
-        date: str,
+        appointment: AppointmentCreate,
         db: Session = Depends(get_db)
 ):
 
-    appointment = Appointment(
-        patient_name=patient_name,
-        doctor_name=doctor_name,
-        date=date
+    new_appointment = Appointment(
+        patient_name=appointment.patient_name,
+        doctor_name=appointment.doctor_name,
+        date=appointment.date
     )
 
-    db.add(appointment)
-
+    db.add(new_appointment)
     db.commit()
-
-    db.refresh(appointment)
+    db.refresh(new_appointment)
 
     return {"message": "Appointment created"}
 
 
 @router.get("/appointments")
 def get_appointments(db: Session = Depends(get_db)):
-
-    appointments = db.query(Appointment).all()
-
-    return appointments
+    return db.query(Appointment).all()
